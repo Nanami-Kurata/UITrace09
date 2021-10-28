a41a1b3816b27a6c66a722eea71ee050818ce4e0
Nanami Kurata <krtnnm20011031@gmail.com>
2021-10-28 15:52:47 +0900

    UITrace09

---
+++ UITrace09/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  UITrace09
+//
+//  Created by 倉田七海 on 2021/10/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
